9698a4a8f5cdb1c2d3f70f8d9884c4a9a8c04dac
calexandar <alexandercvet243@gmail.com>
2026-06-16 21:17:25 +0200

    changed route for welcome

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
-
-Route::view('/steakhouse', 'steakhouse')->name('steakhouse');
+Route::view('/', 'steakhouse')->name('steakhouse');
